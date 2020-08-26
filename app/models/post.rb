6eb86d89339532299be2dabbb9b275b75005fce6
Felipe <felipeletelierc@gmail.com>
2020-08-26 15:01:24 -0400

    we create the chalenge and create de heroku page

---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :image_url, presence: true
+    validates :content, presence: true
+    
+    before_save :delete_wrong_word
+    def delete_wrong_word
+        if self.content.include?("spoiler")
+            self.content = self.content.gsub("spoiler", "")
+            self.content = self.content.gsub("Spoiler", "")
+        end
+    end
+end
+
